1f3fce564403f1f0fd6abf390d74cb21cb8e9aab
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-06 20:48:59 +0100

    Update Categories.js

--- flavor-trove/src/components/Categories.js
+++ flavor-trove/src/components/Categories.js
@@ -2,32 +2,31 @@ import React, { useEffect, useState } from "react";
 import { fetchCategories } from "../api";
 
 const Categories = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        const fetchCategoriesData = async () => {
-            try {
-                const data = await fetchCategories();
-                setCategories(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  useEffect(() => {
+    const fetchCategoriesData = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategories(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchCategoriesData();
-    }, []);
+    fetchCategoriesData();
+  }, []);
 
-    return (
-        <div>
-            <h1>Categories</h1>
-            <ul>
-                {categories.map((category) => (
-                    <li key={category}>{category}</li>
-                ))}
-            </ul>
-        </div>
-    );
-
+  return (
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
