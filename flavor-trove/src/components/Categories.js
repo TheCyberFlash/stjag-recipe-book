149a366b7394b9d76bf3e4f45b38773d259e007a
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-06 20:45:13 +0100

    Create Categories.js

---
+++ flavor-trove/src/components/Categories.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { fetchCategories } from "../api";
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategoriesData = async () => {
+            try {
+                const data = await fetchCategories();
+                setCategories(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchCategoriesData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Categories</h1>
+            <ul>
+                {categories.map((category) => (
+                    <li key={category}>{category}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
+};
+
+export default Categories;
