ed4f5cf99de4ab58b4477cba916f5bc3795677df
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-06 20:48:46 +0100

    Update api.js

--- flavor-trove/src/api.js
+++ flavor-trove/src/api.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "1576bcc0dd5e9cc7b350349296e59fbb";
 const API_URL = "https://api.edamam.com/api/recipes/v2";
+const API_ID = "5a744d30";
 
 export const fetchCategories = async () => {
-    try {
-        const response = await axios.get(`${API_URL}/types`, {
-            params: {
-                app_id: API_ID,
-                app_key: API_KEY
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(`${API_URL}/types`, {
+      params: {
+        app_id: API_ID,
+        app_key: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
